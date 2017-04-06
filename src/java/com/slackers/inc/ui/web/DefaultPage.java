2602e43a6683fa178f4fb77760c734557d0d83be
JohnsNewHP\John's New HP <jtstegeman@wpi.edu>
2017-04-06 19:05:40 -0400

    Initial commit..

---
+++ src/java/com/slackers/inc/ui/web/DefaultPage.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.slackers.inc.ui.web;
+
+/**
+ *
+ * @author John Stegeman <j.stegeman@labyrinth-tech.com>
+ */
+public class DefaultPage implements IPageFrame{
+    
+    private String body;
+    private String title;
+    
+    public DefaultPage(String title, String bodyContent)
+    {
+        this.title = title;
+        this.body = bodyContent;
+    }
+    
+    public DefaultPage(String title)
+    {
+        this(title, "");
+    }
+
+    @Override
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String getNavBar() {
+        return 
+"<nav class=\"navbar navbar-default\">\n" +
+"  <div class=\"container-fluid\">\n" +
+"    <div class=\"navbar-header\">\n" +
+"      <a class=\"navbar-brand\" href=\""+WebComponentProvider.WEB_ROOT+"\">COLA Search Registry</a>\n" +
+"    </div>\n" +
+"    <ul class=\"nav navbar-nav\">\n" +
+"      <li class=\"active\"><a href=\""+WebComponentProvider.WEB_ROOT+"\">Home</a></li>\n" +
+"      <li class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Page 1 <span class=\"caret\"></span></a>\n" +
+"        <ul class=\"dropdown-menu\">\n" +
+"          <li><a href=\"#\">Page 1-1</a></li>\n" +
+"          <li><a href=\"#\">Page 1-2</a></li>\n" +
+"          <li><a href=\"#\">Page 1-3</a></li>\n" +
+"        </ul>\n" +
+"      </li>\n" +
+"      <li><a href=\""+WebComponentProvider.WEB_ROOT+"\">Info</a></li>\n" +
+"    </ul>\n" +
+"    <ul class=\"nav navbar-nav navbar-right\">\n" +
+"      <li><a href=\""+WebComponentProvider.WEB_ROOT+"account/signup\"><span class=\"glyphicon glyphicon-user\"></span> Sign Up</a></li>\n" +
+"      <li><a href=\""+WebComponentProvider.WEB_ROOT+"account/login\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a></li>\n" +
+"    </ul>\n" +
+"  </div>\n" +
+"</nav>";
+        
+    }
+    
+    
+}
