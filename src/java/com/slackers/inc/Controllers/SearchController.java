0b3e3d34e41bce999fc318a7a4b3b8cda747cad7
Ryan McA'Nulty <ryan@mcanulty.org>
2017-04-07 16:44:25 -0400

    starting keywordSearch function

--- src/java/com/slackers/inc/Controllers/SearchController.java
+++ src/java/com/slackers/inc/Controllers/SearchController.java
@@ -48,5 +48,11 @@ public class SearchController {
         System.out.println(String.join(", ", columns));
         return db.getAllEntites_Typed(target, columns.toArray(new String[columns.size()]));
     }
+    public static void keywordSearch() {
+        String keywords = "hi -bye";
+        String parts[] = keywords.split(" -");
+        System.out.println("contains: " + parts[0]);
+        System.out.println("does not contain: " + parts[1]);
+    }
 
 }
