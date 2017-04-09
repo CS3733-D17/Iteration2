631a8f0a832880faa8d69a92c311afec5515c4ff
FABIOPC\Fabio <fborges@wpi.edu>
2017-04-09 17:22:15 -0400

    Added display stub and loadImage stub to Image classes

--- src/java/com/slackers/inc/database/ProxyLabelImage.java
+++ src/java/com/slackers/inc/database/ProxyLabelImage.java
@@ -11,8 +11,24 @@ package com.slackers.inc.database;
  */
 public class ProxyLabelImage implements ILabelImage
 {
-    public ProxyLabelImage()
+    private RealLabelImage realLabel;
+    private String fileName;
+    
+    public ProxyLabelImage(String fileName)
     {
-
+        this.fileName = fileName;
+    }
+    
+    public String getFileName()
+    { return fileName; }
+    
+    // Load the real image if it has not been loaded and display that image
+    //@Override
+    public void display()
+    {
+        if(realLabel == null)
+            realLabel = new RealLabelImage(fileName);
+        
+        realLabel.display();
     }
 }
