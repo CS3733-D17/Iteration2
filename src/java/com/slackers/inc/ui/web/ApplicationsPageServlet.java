abba5c658c3b6a2754e2adfdd495ec927e6a324d
LEEDLE_MACHINE\Jason <jabel@wpi.edu>
2017-04-09 23:27:50 -0400

    trying to fix application page stuff

--- src/java/com/slackers/inc/ui/web/ApplicationsPageServlet.java
+++ src/java/com/slackers/inc/ui/web/ApplicationsPageServlet.java
@@ -35,16 +35,8 @@ public class ApplicationsPageServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ApplicationsPageServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ApplicationsPageServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            IPageFrame pg = WebComponentProvider.getCorrectFrame(request, "applicationPage");
+            out.println(WebComponentProvider.buildPage(pg, request));
         }
     }
 
