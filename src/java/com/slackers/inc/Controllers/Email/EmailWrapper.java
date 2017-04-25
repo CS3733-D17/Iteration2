14d0b0c9bdf350283a6252bd7a219568cc492ff7
Jeffrey Estrada <jestrada@wpi.edu>
2017-04-25 12:54:59 -0400

    Emails are working

--- src/java/com/slackers/inc/Controllers/Email/EmailWrapper.java
+++ src/java/com/slackers/inc/Controllers/Email/EmailWrapper.java
@@ -8,37 +8,55 @@ package com.slackers.inc.Controllers.Email;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.lang.NullPointerException;
 
 /**
  *
  * @author jestrada
  */
 public class EmailWrapper {
-    String to;
+    String[] to;
     String from;
+    String[] cc;
     String pwd;
     String sub; // Could probably set it to an enum
     String msg;
+    MimeMessage message;
     Session session;
     
     Properties props;
     
-    EmailWrapper _email;
+//    EmailWrapper _email;
+    
+//    private EmailWrapper(){
+//        this.to[0] = "";
+//        this.from = "";
+//        this.pwd = "";
+//        this.sub = "";
+//        this.msg = "";
+//        
+//        this.session = Session.getDefaultInstance(props,    
+//           new javax.mail.Authenticator() {    
+//           protected PasswordAuthentication getPasswordAuthentication() {    
+//           return new PasswordAuthentication(from,pwd);  
+//           }    
+//          });    
+//        
+//        this.props = new Properties();
+//        props.put("mail.smtp.host", "smtp.gmail.com");    
+//        props.put("mail.smtp.socketFactory.port", "465");    
+//        props.put("mail.smtp.socketFactory.class",    
+//                        "javax.net.ssl.SSLSocketFactory");    
+//        props.put("mail.smtp.auth", "true");    
+//        props.put("mail.smtp.port", "465");
+//    
+//    }
 
-    private EmailWrapper(String to, String from, String pwd, String sub, String msg){
+    public EmailWrapper(String from, String pwd, String sub, String msg, String... to){
         this.to = to;
         this.from = from;
         this.pwd = pwd;
         this.sub = sub;
         this.msg = msg;
-        
-        this.session = Session.getDefaultInstance(props,    
-           new javax.mail.Authenticator() {    
-           protected PasswordAuthentication getPasswordAuthentication() {    
-           return new PasswordAuthentication(from,pwd);  
-           }    
-          });    
         
         this.props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");    
@@ -47,38 +65,101 @@ public class EmailWrapper {
                         "javax.net.ssl.SSLSocketFactory");    
         props.put("mail.smtp.auth", "true");    
         props.put("mail.smtp.port", "465");
+        
+        // This  part doesn't work
+        this.session = Session.getDefaultInstance(props,    
+            new javax.mail.Authenticator() {    
+                //@Override
+                protected PasswordAuthentication getPasswordAuthentication() {    
+                    return new PasswordAuthentication(from,pwd);  
+                }    
+            });    
+
+        // ---------------------------
+        message = new MimeMessage(session);
+        
+        
     }
     
-    public EmailWrapper setEmail(String to, String from, String pwd, String sub, String msg){
-        if (_email == null){
-            _email = new EmailWrapper(to,from,pwd,sub, msg);
-        } else if( !this.to.equals(to) || !this.from.equals(from) || !this.pwd.equals(pwd) || !this.sub.equals(sub) || this.msg.equals(msg)){
-            _email = new EmailWrapper(to,from,pwd,sub, msg);
-        } else {
-            System.out.println("EmailWrapper already exist");
-        }
-        return _email;
-    } 
+    public EmailWrapper(String from, String pwd, String sub, String msg,String[] cc, String... to){
+        this.to = to;
+        this.from = from;
+        this.pwd = pwd;
+        this.sub = sub;
+        this.msg = msg;
+        this.cc = cc;
+        
+        
+        this.session = Session.getDefaultInstance(props,    
+            new javax.mail.Authenticator() {    
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {    
+                    return new PasswordAuthentication(from,pwd);  
+                }    
+            });    
+        
+        message = new MimeMessage(session);
+        
+        this.props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");    
+        props.put("mail.smtp.socketFactory.port", "465");    
+        props.put("mail.smtp.socketFactory.class",    
+                        "javax.net.ssl.SSLSocketFactory");    
+        props.put("mail.smtp.auth", "true");    
+        props.put("mail.smtp.port", "465");    
+    }
+    
+    
+    
+//    public EmailWrapper getEmail(){
+//        
+//        return _email;
+//    }
+    
+//    public EmailWrapper setEmail(String from, String pwd, String sub, String msg, String... to){
+//        if (_email == null){
+//            _email = new EmailWrapper(from,pwd,sub, msg, to);
+//        } else if( !this.to.equals(to) || !this.from.equals(from) || !this.pwd.equals(pwd) || !this.sub.equals(sub) || this.msg.equals(msg)){
+//            _email = new EmailWrapper(from,pwd,sub, msg, to);
+//        } else {
+//            System.out.println("EmailWrapper already exist");
+//        }
+//        return _email;
+//    } 
+    
+   
     
     public void sendEmail() throws Exception{
-        if (_email == null){
-            System.out.println("Email Wrapper is not set up yet");
-            throw new NullPointerException(); // Not sure if appropriate exception
-        } else {
-            try {    
-                MimeMessage message = new MimeMessage(session);    
-                message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
-                message.setSubject(sub);    
-                message.setText(msg);    
-                //send message  
-                Transport.send(message);    
-                System.out.println("message sent successfully");    
-                } catch (MessagingException e) {
-                    throw new RuntimeException(e);
-                }    
-        }
-    }
+
+        try {    
+            // Checks if theres anyone to send to
+            // Assume that inputs are actual emails
+            if (to.length > 0){
+                for (String to1 : to) {
+                    
+                    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to1)); 
+                }
+            }else {
+                System.out.println("No recipient available");
+                throw new MessagingException();
+            }   
+            
+            if (cc != null && cc.length > 0){
+                for (String cc1 : cc){
+                    message.addRecipient(Message.RecipientType.CC, new InternetAddress(cc1));
+                }
+            }
+            message.setSubject(sub);    
+            message.setText(msg);    
+            //send message  
+            Transport.send(message);    
+            System.out.println("message sent successfully");    
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }    
+    }    
+    
     
 }
-  
+
      
